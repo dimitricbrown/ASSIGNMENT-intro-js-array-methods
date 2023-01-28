247414606420f34819870f432e0ab2754a1ace0a
dimitricbrown <dimitricbrown@outlook.com>
2023-01-28 13:17:44 -0600

    Toggled cart and calculated cart total

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,13 @@ const renderCards = (array) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+   const [, id] = event.target.id.split('--');
+   
+   const index = referenceList.findIndex(item => item.id === Number(id))
+
+   referenceList[index].inCart = !referenceList[index].inCart
+   cartTotal();
+   renderCards(referenceList);
   }
 }
 
@@ -79,8 +85,16 @@ const buttonFilter = (event) => {
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
+  const cart = referenceList.filter(item => item.inCart);
+  const total = cart.reduce((value1, value2) => value1 + value2.price, 0)
+  const free = cart.some(item => item.price <= 0)
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
+
+  if(free) {
+    document.querySelector('#includes-free').innerHTML = "INCLUDES FREE ITEMS"
+  } else {
+    document.querySelector('#includes-free').innerHTML = ''
+  }
 }
 
 // RESHAPE DATA TO RENDER TO DOM
